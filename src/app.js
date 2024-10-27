c9a41c7b2d359bef7154b48af0bf1f01c4ebe478
Corbin Davenport <me@corbin.io>
2024-10-26 20:15:45 -0400

    Enable video upload support, update index page info

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const uploadLimit = Number(process.env.UPLOAD_LIMIT);
 // Time delay for automatically deleting images, in minutes
 const deleteDelay = (parseInt(process.env.AUTODELETE_TIME, 10) || 2);
 // Default name for image uploads
-const defaultImgTitle = 'ImageShare Upload';
+const defaultFileTitle = 'ImageShare Upload';
 // Paths to primary directories
 const publicDir = path.resolve(import.meta.dirname, '../public');
 const mainDir = path.resolve(import.meta.dirname, '../');
@@ -35,6 +35,8 @@ const json3DS = xmlParser.parse(xmlFile);
 console.log(`
 Domain: ${webDomain}
 Image delete delay: ${deleteDelay} minute(s)
+File upload limit: ${uploadLimit} MB
+Plausible analytics domain: ${(plausibleDomain || 'None')}
 `);
 
 // Create uploads folder, and delete existing one if present
@@ -86,7 +88,7 @@ async function getSoftwareTitle(imgFile, mimeType) {
     'image/jpeg',
     'image/png'
   ];
-  if (!supportedFileTypes.includes(mimeType)) return defaultImgTitle;
+  if (!supportedFileTypes.includes(mimeType)) return defaultFileTitle;
   // Continue reading EXIF data
   const tags = await ExifReader.load(imgFile);
   if (tags['Model']?.description === 'Nintendo 3DS' && tags['Software']?.description) {
@@ -101,7 +103,7 @@ async function getSoftwareTitle(imgFile, mimeType) {
     }
   }
   // Return default software title if none is detected
-  return defaultImgTitle;
+  return defaultFileTitle;
 }
 
 // Function to render header for HTML pages
@@ -130,7 +132,7 @@ function renderHead(userAgent) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>ImageShare</title>
-    <meta name="description" content="ImageShare is a lightweight web app for uploading images, created for the Nintendo 3DS and other legacy web browsers.">
+    <meta name="description" content="ImageShare is a web app for sending images and videos to another device, designed for low-end and legacy web browsers.">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-title" content="ImageShare">
@@ -148,7 +150,7 @@ function renderHead(userAgent) {
     <!-- Open Graph card -->
     <meta property="og:type" content="website" />
     <meta property="og:title" content="ImageShare" />
-    <meta property="og:description" content="ImageShare is a lightweight web app for uploading images, created for the Nintendo 3DS and other legacy web browsers." />
+    <meta property="og:description" content="ImageShare is a web app for sending images and videos to another device, designed for low-end and legacy web browsers." />
     <meta property="og:image:width" content="512" />
     <meta property="og:image:height" content="512" />
     <meta property="og:url" content="https://${webDomain}" />
@@ -160,7 +162,7 @@ function renderHead(userAgent) {
 }
 
 
-function renderMain(userAgent = '', uploadUrl = '', secure = false, softwareTitle = defaultImgTitle) {
+function renderMain(userAgent = '', uploadUrl = '', secure = false, softwareTitle = defaultFileTitle) {
   // Render initial header elements
   // Background color is defined in <body> attributes for ancient browsers, like Netscape 4.x
   let htmlString = `<!DOCTYPE html>
@@ -184,7 +186,7 @@ function renderMain(userAgent = '', uploadUrl = '', secure = false, softwareTitl
             </a>
         </div>
         <div class="body">
-          <p>You have ${deleteDelay} minute(s) to save your image before it is automatically deleted.</p>
+          <p>You have ${deleteDelay} minute(s) to save your file before it is deleted.</p>
         </div>
       </div>
     `;
@@ -196,7 +198,7 @@ function renderMain(userAgent = '', uploadUrl = '', secure = false, softwareTitl
         <h3 class="panel-title">Upload Image</h3>
         <div class="body">
           <form action="/" id="upload-form" enctype="multipart/form-data" method="POST" onsubmit="document.getElementById('loading-container').style.display='block';">
-            <p><input name="img" id="img-btn" type="file" accept="image/*" /></p>
+            <p><input name="img" id="img-btn" type="file" accept="image/*,video/*" /></p>
             <p><input name="submit" type="submit" value="Upload" /></p>
             <p id="loading-container" style="display:none;" align="center">
               <img src="/img/loading.gif" alt="Loading">
@@ -204,13 +206,12 @@ function renderMain(userAgent = '', uploadUrl = '', secure = false, softwareTitl
             <p>Maximum file size: ${uploadLimit} MB</p>
           </form>
           <hr>
-          <p>ImageShare is a lightweight web app for uploading images with QR codes, created for the Nintendo 3DS and other legacy web browsers. See <a href="https://github.com/corbindavenport/imageshare" target="_blank">tinyurl.com/imgsharegit</a> for more information.</p>
+          <p>ImageShare is a web app for sending images and videos to another device, designed for low-end and legacy web browsers. ImageShare is open-source software, see the below links for more information and support.</p>
+          <p style="text-align: center; font-weight: bold;"><a href="https://github.com/corbindavenport/imageshare" target="_blank">github.com/corbindavenport/imageshare</a></p>
+          <p style="text-align: center; font-weight: bold;"><a href="https://discord.gg/tqJDRsmQVn" target="_blank">discord.gg/tqJDRsmQVn</a></p>
           <p>If you find ImageShare useful, please consider donating to support development and server costs!</p>
           <p style="text-align: center; font-weight: bold;"><a href="https://www.patreon.com/corbindavenport" target="_blank">patreon.com/corbindavenport</a></p>
           <p style="text-align: center; font-weight: bold;"><a href="https://cash.app/$corbdav" target="_blank">cash.app/$corbdav</a> • <a href="https://paypal.me/corbindav" target="_blank">paypal.me/corbindav</a></p>
-          <hr />
-          <p>Join Discord server: <a href="https://discord.gg/tqJDRsmQVn" target="_blank">discord.gg/tqJDRsmQVn</a></p>
-          <p>Follow on Mastodon: <a href="https://toot.community/@corbin" target="_blank">@corbin@toot.community</a>
         </div>
       </div>
     </div>
@@ -230,7 +231,7 @@ app.post('*', upload.single('img'), async function (req, res, err) {
     // Detect software title
     const softwareTitle = await getSoftwareTitle(req.file.path, req.file.mimetype);
     // If custom software title is detected, run exiftool to save it to the image description
-    if (softwareTitle != defaultImgTitle) {
+    if (softwareTitle != defaultFileTitle) {
       spawn('exiftool', [`-Caption-Abstract=${softwareTitle}`, `-ImageDescription=${softwareTitle}`, req.file.path]);
     }
     // Schedule timeout to delete image
