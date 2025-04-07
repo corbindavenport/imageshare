7f0fba9b0f67040c2f938650ed6fac97ab61f706
Corbin Davenport <me@corbin.io>
2025-04-06 22:03:50 -0400

    Fix typo in local uploads

--- src/modules/local-upload.js
+++ src/modules/local-upload.js
@@ -37,7 +37,7 @@ async function uploadToLocal(filePath, protocol, connectedHost, req, plausibleDo
             sendAnalytics(req.get('User-Agent'), (req.headers['x-forwarded-for'] || req.ip), data);
         }
         // Set the footer message for qr panel
-        const qrFooter = `Scan the QR code or type the link on another device to download the file. You have ${deleteDelay} ${deleteDelay === 1 ? 'minute' : 'minutes'} minutes to save your file before it is deleted.`;
+        const qrFooter = `Scan the QR code or type the link on another device to download the file. You have ${deleteDelay} ${deleteDelay === 1 ? 'minute' : 'minutes'} to save your file before it is deleted.`;
         // Return success and display results to user :3
         resolve({ success: true, link: `${protocol}://${connectedHost}/i/${shortLink}`, qrLink: `${filePath.path.replace('uploads/', '/qr/')}`, qrFooter });
     });
