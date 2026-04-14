91d76c88e80aa9803f4371bab714ae04f93d5064
Corbin Davenport <corbindavenport@outlook.com>
2026-04-14 19:51:17 -0400

    Remove unnecessary Promise from local upload function

--- src/modules/local-upload.js
+++ src/modules/local-upload.js
@@ -7,34 +7,32 @@ const shortLinkObj = {};
 
 // Function to handle local uploads
 async function uploadToLocal(uploadData) {
-    return new Promise((resolve) => {
-        let shortLink;
-        do {
-            shortLink = crypto.randomUUID().toString().substring(0, 5);
-        } while (shortLinkObj[shortLink]);
-        shortLinkObj[shortLink] = uploadData.relativePath;
-        console.log(`Created shortlink: ${uploadData.origin}/i/${shortLink}`);
-        // Schedule timeout to delete file and shortlink
-        const delay = uploadData.deleteDelay * 60 * 1000;
-        setTimeout(async () => {
-            // Delete shortlink
-            delete shortLinkObj[shortLink];
-            console.log(`Deleted shortlink: ${uploadData.origin}/i/${shortLink}`);
-            // Delete file
-            if (uploadData.absolutePath) {
-                fs.unlinkSync(uploadData.absolutePath);
-                console.log(`Deleted file: ${uploadData.absolutePath}`);
-            }
-        }, delay);
-        // Return upload data
-        const response = {
-            success: true,
-            link: `${uploadData.origin}/i/${shortLink}`,
-            qrLink: getQrLink(`${uploadData.origin}/${uploadData.relativePath}`),
-            qrFooter: `Scan the QR code on another device to download the file, or type the link. You have ${uploadData.deleteDelay} ${uploadData.deleteDelay === 1 ? 'minute' : 'minutes'} to save your file before it is deleted.`
+    let shortLink;
+    do {
+        shortLink = crypto.randomUUID().toString().substring(0, 5);
+    } while (shortLinkObj[shortLink]);
+    shortLinkObj[shortLink] = uploadData.relativePath;
+    console.log(`Created shortlink: ${uploadData.origin}/i/${shortLink}`);
+    // Schedule timeout to delete file and shortlink
+    const delay = uploadData.deleteDelay * 60 * 1000;
+    setTimeout(async () => {
+        // Delete shortlink
+        delete shortLinkObj[shortLink];
+        console.log(`Deleted shortlink: ${uploadData.origin}/i/${shortLink}`);
+        // Delete file
+        if (uploadData.absolutePath) {
+            fs.unlinkSync(uploadData.absolutePath);
+            console.log(`Deleted file: ${uploadData.absolutePath}`);
         }
-        resolve(response);
-    });
+    }, delay);
+    // Return upload data
+    const response = {
+        success: true,
+        link: `${uploadData.origin}/i/${shortLink}`,
+        qrLink: getQrLink(`${uploadData.origin}/${uploadData.relativePath}`),
+        qrFooter: `Scan the QR code on another device to download the file, or type the link. You have ${uploadData.deleteDelay} ${uploadData.deleteDelay === 1 ? 'minute' : 'minutes'} to save your file before it is deleted.`
+    }
+    return response;
 }
 
 export { uploadToLocal, shortLinkObj };
